8710731cf0269452a461d4744e6f474e43cc9db9
Martín Ghiotti <martin.ghiotti@innovavista.net>
2021-03-23 19:12:16 -0300

    Migracion de usuarios root

--- database/phinx/20210315231431_agregar_roles.php
+++ database/phinx/20210315231431_agregar_roles.php
@@ -7,13 +7,14 @@ final class AgregarRoles extends AbstractMigration
 {
     public function up(): void {
         $hoy = \Carbon\Carbon::now()->format('Y-m-d H:i');
-        $this->execute("INSERT INTO roles VALUES(null, 'root', 'Root', 'Rol con todos los permisos', 1, 1, '$hoy', '$hoy', null, null);");
-        $this->execute("INSERT INTO roles VALUES(null, 'invitado', 'Invitado', 'Usuario que no ha sido autenticado', 0, 1, '$hoy', '$hoy', null, null);");
-        $this->execute("INSERT INTO roles VALUES(null, 'admin', 'Administrador', 'Administrador del sistema', 0, 1, '$hoy', '$hoy', null, null);");
-        $this->execute("INSERT INTO roles VALUES(null, 'comensal', 'Comensal', 'Persona que puede realizar pedidos en la web', 0, 1, '$hoy', '$hoy', null, null);");
+        $this->execute("INSERT INTO roles VALUES(default, 'root', 'Root', 'Rol con todos los permisos', 1, 1, '$hoy', '$hoy', null, null);");
+        $this->execute("INSERT INTO roles VALUES(default, 'admin', 'Administrador', 'Administrador del sistema. Se encarga del ingreso y creación de usuarios', 0, 1, '$hoy', '$hoy', null, null);");
+        $this->execute("INSERT INTO roles VALUES(default, 'mozo', 'Mozo', 'Persona que puede gestiona las mesas', 0, 1, '$hoy', '$hoy', null, null);");
+        $this->execute("INSERT INTO roles VALUES(default, 'comensal', 'Comensal', 'Usuario que realiza pedidos online', 0, 1, '$hoy', '$hoy', null, null);");
     }
 
     public function down(): void {
+        $this->execute("DELETE FROM usuario_rol;");
         $this->execute("DELETE FROM roles;");
     }
 }
---
+++ database/phinx/20210323220833_usuarios_root.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class UsuariosRoot extends AbstractMigration
+{
+    public function up(): void {
+        $hoy = \Carbon\Carbon::now()->format('Y-m-d H:i');
+        $this->execute("INSERT INTO usuarios VALUES (default, 'Martín', 'martinghiotti2013@gmail.com', null, null, null, 1, '$10$0hakHJWsP2G1aMeqJj0NHumx9rZyMPlDSUqBLt5r6rA2yJjDQZujO', '$hoy', '$hoy', null, null);");
+        $this->execute("INSERT INTO usuarios VALUES (default, 'Bernardo', 'bernardopolidoro@gmail.com', null, null, null, 1, '$10$0hakHJWsP2G1aMeqJj0NHumx9rZyMPlDSUqBLt5r6rA2yJjDQZujO', '$hoy', '$hoy', null, null);");
+        $this->execute("INSERT INTO `proyecto`.`usuario_rol` VALUES (
+                (SELECT id FROM roles WHERE nombre = 'root'),
+                (SELECT id FROM usuarios WHERE nombre = 'Martín')
+            )
+        ");
+        $this->execute("INSERT INTO `proyecto`.`usuario_rol` VALUES (
+                (SELECT id FROM roles WHERE nombre = 'root'),
+                (SELECT id FROM usuarios WHERE nombre = 'Bernardo')
+            )
+        ");
+    }
+
+    public function down(): void {
+
+    }
+}
