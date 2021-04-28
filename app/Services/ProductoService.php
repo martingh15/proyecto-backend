3141de2e81523b34cada5d19088e22f38f477f5f
martingh15 <martinghiotti@yahoo.com.ar>
2021-04-28 18:35:04 -0300

    Agrego use de Validator faltante

--- app/Services/ProductoService.php
+++ app/Services/ProductoService.php
@@ -8,9 +8,9 @@ use App\Resultado\Resultado;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use Intervention\Image\Facades\Image;
 use Throwable;
-use Validator;
 
 class ProductoService
 {
